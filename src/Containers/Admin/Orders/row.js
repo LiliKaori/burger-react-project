966f3c3a802cbdf8454a3e566f994790115fefaf
LiliKaori <lilikaori10@gmail.com>
2023-04-20 11:28:34 -0300

    Seletor de status e menu lateral

--- src/Containers/Admin/Orders/row.js
+++ src/Containers/Admin/Orders/row.js
@@ -16,7 +16,7 @@ import api from '../../../services/api'
 import status from './order-status'
 import { PorductsImg, ReactSelectStyle } from './styles'
 
-function Row({ row }) {
+function Row({ row, orders, setOrders }) {
   const [open, setOpen] = React.useState(false)
   const [isLoading, setIsLoading] = React.useState(false)
 
@@ -24,6 +24,10 @@ function Row({ row }) {
     setIsLoading(true)
     try {
       await api.put(`orders/${id}`, { status })
+      const newOrders = orders.map(order => {
+        return order._id === id ? { ...order, status } : order
+      })
+      setOrders(newOrders)
     } catch (err) {
       console.error(err)
     } finally {
@@ -50,7 +54,7 @@ function Row({ row }) {
         <TableCell>{row.date}</TableCell>
         <TableCell>
           <ReactSelectStyle
-            options={status}
+            options={status.filter(sts => sts.value !== 'Todos')}
             menuPortalTarget={document.body}
             placeholder="Status"
             defaultValue={
@@ -106,6 +110,8 @@ function Row({ row }) {
 }
 
 Row.propTypes = {
+  orders: PropTypes.array,
+  setOrders: PropTypes.func,
   row: PropTypes.shape({
     name: PropTypes.string.isRequired,
     orderId: PropTypes.string.isRequired,
--- src/Containers/Admin/index.js
+++ src/Containers/Admin/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import { SiteMenuAdmin } from '../../components'
 import Orders from './Orders'
 import { Container } from './styles'
 
 export function Admin() {
   return (
     <Container>
+      <SiteMenuAdmin />
       <Orders />
     </Container>
   )
